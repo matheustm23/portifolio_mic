b432e541c43485e4920c04ff69a3e769f9712439
matheustm23 <matheus.tm@aluno.ifsc.edu.br>
2026-02-27 16:45:52 -0300

    Adicionado delay

--- aula1_estrutura_projeto/src/gpio.c
+++ aula1_estrutura_projeto/src/gpio.c
@@ -1,6 +1,8 @@
 //Autor: Matheus
+#define F_CPU 16000000
 #include "gpio.c"
 #include <xc.h>
+#include "util/delay.h"
 
 void GPIO_initialize();
 {
@@ -9,5 +11,8 @@ void GPIO_initialize();
 
 void GPIO_blinked();
 {
-	PORTB = (1<<PORTB0); //aciona PB0
+	PORTB |= (1<<PORTB0); //aciona PB0
+	_delay_ms(300);
+	PORTB &= ~(1<<PORTB0); //desaciona PB0
+	_delay_ms(700);
 }
