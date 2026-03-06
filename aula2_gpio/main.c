b798e8bb65cf8c4cff1cc95d916347e5b08cb150
matheustm23 <matheus.tm@aluno.ifsc.edu.br>
2026-03-06 16:23:06 -0300

    Piscando Led Sequencial

--- aula2_gpio/main.c
+++ aula2_gpio/main.c
@@ -9,7 +9,7 @@
 #include <xc.h>
 #include "util/delay.h"
 
-/*@brief O que este código faz:
+/* @brief O que este código faz:
 Pisca um led ligado ao pino PC2 enquanto a 
 chave ligada ao pino PD3 estiver pressionada
 */
@@ -40,7 +40,37 @@ void atividade1()
 	}
 }
 
+/* @brief Controla 6 leds (PB0 a PB5) em 
+uma logica de pinheirinho de natal */
+
+void atividade2()
+{
+	//Configurando pinos PB0-PB5 como saida
+	DDRB |= ((1<<DDB5) | (1<<DDB4) | (1<<DDB3) | (1<<DDB2) | (1<<DDB1) | (1<<DDB0));
+	uint8_t tState =1; //Guarda estado de acionamento dos leds
+	while(1)
+	{
+		tState = tState << 1;
+		if(tState == 0b01000000)
+		{
+			tState=1;
+		}
+		
+		if((PIND & (1<<PIND3))!=0)
+		{
+			PORTB =tState;
+		}
+		
+		else
+		{
+			//Chave aberta
+			PORTB =0;
+		}
+		_delay_ms(200);
+	}
+}
+
 int main(void)
 {
-    atividade1();
+	atividade2();
 }
