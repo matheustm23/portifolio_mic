299b6e8006bd1cb2c1d1355c8cc853faf03ac427
matheustm23 <matheus.tm@aluno.ifsc.edu.br>
2026-03-13 15:13:19 -0300

    Inclusão da interrupção INT1 na aula 3 Também adicionado um flag de interrupção

--- aula3_interrupts/main.c
+++ aula3_interrupts/main.c
@@ -1,6 +1,5 @@
 ﻿/*
  * main.c
- *
  * Created: 3/13/2026 2:22:41 PM
  *  Author: Matheus T Machado
  */ 
@@ -9,18 +8,34 @@
 #include "avr/interrupt.h"
 #include "util/delay.h"
 
+int gCount = 0;
+
 ISR(INT0_vect) 
 {
-	PORTC ^=(1<<PORTC0); //Alterna Led
+	PORTC ^=(1<<PORTC0);
 	_delay_ms(100);
 	PORTC=0;
+}
+
+ISR(INT1_vect)
+{
+	gCount++;
+	if(gCount == 3)
+	{
+		PORTC ^=(1<<PORTC0);
+		_delay_ms(100);
+		PORTC=0;
+		gCount=0;
+	}
+	
 }
 
 int main(void)
 {
 	DDRC =(1<<DDC0); //configura pino PC0 como saída;
-	EICRA = (1<<ISC01)|(1<<ISC00); //configurando a solicitação de interrupção: borda de subida
-	EIMSK = (0<<INT1)|(1<<INT0); //INT0 habilitada, INT1 desabilitada
+	EICRA = (0<<ISC01)|(1<<ISC00) //INT0 configurado pra ambas as bordas
+	      | (1<<ISC11)|(1<<ISC10); //INT1 configurado pra borda de subida
+	EIMSK = (1<<INT1)|(1<<INT0); //INT1 habilitada, INT0 desabilitada
 	sei(); //habilita as interrupções globais
 	
     while(1)
