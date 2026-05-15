81f3d8c0c7bdfe2d1fad42cdcd412e80ed60791d
matheustm23 <matheus.tm@aluno.ifsc.edu.br>
2026-05-15 16:04:05 -0300

    implementação de PWM com interrupções do timer 0

--- aula7_timer/main.c
+++ aula7_timer/main.c
@@ -8,9 +8,14 @@
 #include <xc.h>
 #include "avr/interrupt.h"
 
-ISR(TIMER0_OVF_vect)
+ISR(TIMER0_COMPA_vect)
 {
-	PORTB ^= (1<<PORTB0); //Alterna PB0
+	PORTB |= (1<<PORTB0); //set PB0
+}
+
+ISR(TIMER0_COMPB_vect)
+{
+	PORTB &= ~(1<<PORTB0); //reset PB0
 }
 
 int main(void)
@@ -19,7 +24,8 @@ int main(void)
 	TCCR0A = (1<<WGM01)|(0<<WGM00); //Modo CTC
 	TCCR0B = (1<<WGM02)|(0<<CS02)|(1<<CS01)|(0<<CS00); //Prescaler de 8
 	OCR0A = 249; //Contagem até 250us, assumindo clock de 1MHz 
-	TIMSK0 = (1<<TOIE0); //Habilita interrupção de overflow do timer 0
+	OCR0B = 99; //transição da onda, razão de 40%
+	TIMSK0 = (1<<OCIE0A)|(1<<OCIE0B); //Habilita interrupção A e B de comparção de saída
 	sei();
 	
     while(1)
